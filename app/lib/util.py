a121bb32fb11b513ef7dd6b99af24c36b331f089
tani <ysolakey1116@gmail.com>
2023-03-18 16:09:30 +0900

    ?

---
+++ app/lib/util.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import requests
+import io
+import numpy as np
+
+def openImg(url):
+    pil_img = Image.open(io.BytesIO(requests.get(url).content))
+    return np.array(pil_img)
