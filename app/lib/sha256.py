73ef37350cdf61d17d5a215a69cc85229c3386ea
tani <ysolakey1116@gmail.com>
2023-04-04 22:42:24 +0900

    feat: 🎸 sha256をdictからじゃなくてstrから取得できるように(commit順まちがった)

--- app/lib/sha256.py
+++ app/lib/sha256.py
@@ -1,7 +1,4 @@
 import hashlib
 
 def sha256(target):
-    result = ""
-    for key in target.keys():
-        result = result + str(key) + str(target[key])
-    return hashlib.sha256(result.encode()).hexdigest()
+    return hashlib.sha256(target.encode()).hexdigest()
