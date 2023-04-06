4a6ef1e451525ebd23041eb6c8feec140fd61f4d
tani <ysolakey1116@gmail.com>
2023-04-06 14:25:24 +0900

    feat: 🎸 初期データを突っ込むやつを書き直し

--- app/test_data.py
+++ app/test_data.py
@@ -19,43 +19,34 @@ import json
 from lib.sha256 import sha256
 
 def set_data(db):
-    db.query(models.Difficult).delete()
-    db.commit()
     db.query(models.Music).delete()
     db.commit()
 
-    music_json = requests.get('http://localhost:8080/static/data/old-music.json').text
-    difficult_json = requests.get('http://localhost:8080/static/data/old-difficult.json').text
-    use_music_items = ['id', 'title', 'pronunciation', 'creator', 'lyricist', 'composer', 'arranger']
-    use_difficult_items = ['id', 'musicId', 'musicDifficulty', 'playLevel', 'totalNoteCount']
+    data_json = requests.get('http://localhost:8080/static/data/old-data.json').text
+    items = ['id', 'title', 'pronunciation', 'creator', 'lyricist', 'composer', 'arranger', "level_easy","level_normal","level_hard","level_expert","level_master","totalNote_easy","totalNote_normal","totalNote_hard","totalNote_expert","totalNote_master"]
 
-    for music in json.loads(music_json):
+    for data in json.loads(data_json):
         target = ''
-        for i in use_music_items:
-            target += str(music[i])
+        for i in items:
+            target += str(data[i])
         item = models.Music(
-            id = music['id'],
-            title = music['title'],
-            pronunciation = music['pronunciation'],
-            creator = music['creator'],
-            lyricist = music['lyricist'],
-            composer = music['composer'],
-            arranger = music['arranger'],
-            hash = sha256(target)
-        )
-        db.add(item)
-    db.commit()
-
-    for difficult in json.loads(difficult_json):
-        target = ''
-        for i in use_difficult_items:
-            target += str(difficult[i])
-        item = models.Difficult(
-            id = difficult['id'],
-            musicId = difficult['musicId'],
-            musicDifficulty = difficult['musicDifficulty'],
-            playLevel = difficult['playLevel'],
-            totalNoteCount = difficult['totalNoteCount'],
+            id = data['id'],
+            title = data['title'],
+            pronunciation = data['pronunciation'],
+            creator = data['creator'],
+            lyricist = data['lyricist'],
+            composer = data['composer'],
+            arranger = data['arranger'],
+            level_easy = data['level_easy'],
+            level_normal = data['level_normal'],
+            level_hard = data['level_hard'],
+            level_expert = data['level_expert'],
+            level_master = data['level_master'],
+            totalNote_easy = data['totalNote_easy'],
+            totalNote_normal = data['totalNote_normal'],
+            totalNote_hard = data['totalNote_hard'],
+            totalNote_expert = data['totalNote_expert'],
+            totalNote_master = data['totalNote_master'],
             hash = sha256(target)
         )
         db.add(item)
