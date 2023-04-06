060afb509c83884d2e98e900214f1682e0bbe583
tani <ysolakey1116@gmail.com>
2023-04-06 15:21:40 +0900

    feat: 🎸 差分を検出するエンドポイントが完成

--- app/main.py
+++ app/main.py
@@ -45,28 +45,71 @@ def set_test_data(db: Session = Depends(get_db)):
     set_data(db)
     return {}
 
-@app.get("/music")
+@app.get("/load_diff")
 def get_music(db: Session = Depends(get_db)):
-
     # httpから取得
     music_json = requests.get('http://localhost:8080/static/data/music.json')
+    difficult_json = requests.get('http://localhost:8080/static/data/difficult.json')
 
-    # データの整形
-    ids = []
-    data = {}
-    use_items = ['id', 'title', 'pronunciation', 'creator', 'lyricist', 'composer', 'arranger']
-    for i in json.loads(music_json.text):
-        target = ''
-        data[i['id']] = {}
-        for item in use_items:
-            target += str(i[item])
-            data[i['id']][item] = i[item]
-        ids.append(i['id'])
-        data[i['id']]['hash'] = sha256(target)
-
-    # idの重複がないかバリデーション
-    if has_duplicates(ids):
+    # バリデーション
+    musicIds = list(set([j['id'] for j in json.loads(music_json.text)]))
+    valid_difficulties = {'easy', 'normal', 'hard', 'expert', 'master'}
+  
+    # musicIdにリストmusicIdsに入っていないidがある場合をチェック
+    ids = set(item['musicId'] for item in json.loads(difficult_json.text))
+    invalid_ids = ids - set(musicIds)
+    if invalid_ids:
+        print(invalid_ids)
         return { 'ok':False }
+
+    # musicIdとmusicDifficultyの重複をチェック
+    music_data = [(item['musicId'], item['musicDifficulty']) for item in json.loads(difficult_json.text)]
+    duplicates = [x for n, x in enumerate(music_data) if x in music_data[:n]]
+    if duplicates:
+        print('1')
+        return { 'ok':False }
+
+    # musicDifficultyの値のバリデーションをチェック
+    invalid_difficulties = set(item['musicDifficulty'] for item in json.loads(difficult_json.text)) - valid_difficulties
+    if invalid_difficulties:
+        print('2')
+        return { 'ok':False }
+    
+    # idの重複がないかバリデーション
+    if has_duplicates([j['id'] for j in json.loads(music_json.text)]):
+        return { 'ok':False }
+
+    # データの結合
+    result = []
+    for m in json.loads(music_json.text):
+        tmp = {
+            "id": m["id"],
+            "title": m["title"],
+            "pronunciation": m["pronunciation"],
+            "creator": m["creator"],
+            "lyricist": m["lyricist"],
+            "composer": m["composer"],
+            "arranger": m["arranger"]
+        }
+        for d in json.loads(difficult_json.text):
+            if d["musicId"] == m["id"]:
+                tmp["level_" + d["musicDifficulty"]] = d["playLevel"]
+                tmp["totalNote_" + d["musicDifficulty"]] = d["totalNoteCount"]
+        result.append(tmp)
+    
+    # hashの計算
+    items = ['id', 'title', 'pronunciation', 'creator', 'lyricist', 'composer', 'arranger', "level_easy","level_normal","level_hard","level_expert","level_master","totalNote_easy","totalNote_normal","totalNote_hard","totalNote_expert","totalNote_master"]
+    
+    for j in range(len(result)):
+        target = ''
+        for i in items:
+            target += str(result[j][i])
+        result[j]['hash'] = sha256(target)
+    
+    # 整形
+    data = {}
+    for i in result:
+        data[i["id"]] = i
 
     # DBから取得
     music_db = db.query(models.Music).all()
@@ -75,7 +118,7 @@ def get_music(db: Session = Depends(get_db)):
     data2 = {}
     for i in music_db:
         data2[i.id] = i.toDict()
-    
+
     # idの差分をとる
     diff = {
         "deleted": data2.keys() - data.keys(),
@@ -83,14 +126,27 @@ def get_music(db: Session = Depends(get_db)):
         "same": data.keys() & data2.keys(),
         "updated": []
     }
-    
+
     # hashを確認
     for i in diff['same'].copy():
         if data[i]['hash'] != data2[i]['hash']:
             diff['updated'].append(i)
             diff['same'].remove(i)
     
-    return diff
+    # 具体的なデータをin
+    diff2 = {
+        "deleted": [],
+        "added": [],
+        "updated": []
+    }
+    for i in diff['deleted']:
+        diff2['deleted'].append(data2[i])
+    for i in diff['added']:
+        diff2['added'].append(data[i])
+    for i in diff['updated']:
+        diff2['updated'].append({'old': data2[i], 'new': data[i]})
+
+    return diff2
 
 
 @app.get('/ocr/score')
