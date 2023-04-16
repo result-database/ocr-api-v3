e8a4353e8a1dfa0c95317db2b6b8337a843679be
Cosmic-rare <ysolakey1116@gmail.com>
2023-04-16 11:40:01 +0900

    perf: ⚡️ titleもnumpy優先にして高速化

--- app/lib/title.py
+++ app/lib/title.py
@@ -32,38 +32,25 @@ def getTitle(url, psm, border):
     start = time.time()
 
     # to grayscale
-    img2 = Image.new('RGB', (img.shape[1], img.shape[0]))
-    for y in range(img.shape[0]):
-        for x in range(img.shape[1]):
-            r, g, b = img[y][x]
-
-            if r >= border and g >= border and b >= border:
-                color = 255
-            else:
-                color = 0
-
-            img2.putpixel((x, y), (color, color, color))
-
+    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+    mask = np.logical_and(r >= border, np.logical_and(g >= border, b >= border))
+    img[mask] = [255, 255, 255]
+    img[np.logical_not(mask)] = [0, 0, 0]
 
     # get time of do-grayscale
     time_grayscale = time.time() - start
     start = time.time()
 
     # getbox -> crop
-    crop_range = img2.convert('RGB').getbbox()
-    img = np.array(img2.crop(
-        [crop_range[0], crop_range[1], crop_range[2], (crop_range[3]) // 2]
-    ))
+    crop_range = Image.fromarray(img).convert('RGB').getbbox()
+    img = img[crop_range[1] : (crop_range[3]) // 2, crop_range[0] : crop_range[2]]
 
     # create margin
     img = cv2.copyMakeBorder(img, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=[0,0,0])  
 
-    r = img[:, :, 0]
-    g = img[:, :, 1]
-    b = img[:, :, 2]
+    # 白背景に黒文字に変更
+    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
     mask = np.logical_and(r == 255, np.logical_and(g == 255, b == 255))
-
-    # 一括で更新
     img[mask] = [0, 0, 0]
     img[np.logical_not(mask)] = [255, 255, 255]
 
