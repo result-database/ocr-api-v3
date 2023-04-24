069af4b1ce1cf88a4f4647c66c306dd6982b74dc
Cosmic-rare <ysolakey1116@gmail.com>
2023-04-24 17:51:37 +0900

    perf: ⚡️ ゴミを削除

--- app/lib/util.py
+++ app/lib/util.py
@@ -8,14 +8,6 @@ import math
 def has_duplicates(seq):
     return len(seq) != len(set(seq))
 
-def is_float(s):
-    try:
-        float(s)
-    except ValueError:
-        return False
-    else:
-        return True
-
 def openImg(url):
     pil_img = Image.open(io.BytesIO(requests.get(url).content))
     return np.array(pil_img)
