57978f9d48ee782af7b432abcd1bec7e04b9bd05
Cosmic-rare <ysolakey1116@gmail.com>
2023-04-25 17:18:42 +0900

    refactor: 💡 resuest-modelを別ファイルに移動

---
+++ app/reqtypes.py
@@ -0,0 +1,39 @@
+from pydantic import Field, BaseModel
+from typing import Dict
+
+class Music(BaseModel):
+    id: int
+    title: str
+    pronunciation: str
+    creator: str
+    lyricist: str
+    composer: str
+    arranger: str
+    level_easy: int
+    level_normal: int
+    level_hard: int
+    level_expert: int
+    level_master: int
+    totalNote_easy: int
+    totalNote_normal: int
+    totalNote_hard: int
+    totalNote_expert: int
+    totalNote_master: int
+
+class ReqType(BaseModel):
+    url: str = Field(default="http://localhost:8080/static/wide.png")
+    psmScore: int = Field(default=6, enum=[6, 7])
+    psmDifficult: int = Field(default=7, enum=[6, 7])
+    psmTitle: int = Field(default=11, enum=[6, 7, 11])
+    psmJudge: int = Field(default=6, enum=[6, 7])
+    borderTitle: int = Field(default=215, ge=215, le=255)
+    borderJudge: int = Field(default=230, ge=230, le=255)
+    blurScore: bool = Field(default=True)
+    blurDifficult: bool = Field(default=True)
+    blurTitle: bool = Field(default=True)
+    blurJudge: bool = Field(default=True)
+    candidateRatio: float = Field(default=0.3)
+    candidate: bool = Field(default=True)
+
+class ReqType2(BaseModel):
+    data: Dict[str, Music]
